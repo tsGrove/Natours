d115ad1e78940bb4877ec23b214ca401cbed5eb6
Tyler Hargrove <ts.hargrove95@gmail.com>
2024-06-18 14:22:17 -0500

    added packages for securtiy, including xss-clean, helmet, mongoSanitize, and hpp, cookie settings in the authController, deactivating user funcionality, adjusting query results based on "active" trait of users,

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const AppError = require("./natours/utils/appError");
 const globalErrorHandler = require("./natours/controllers/errorController");
@@ -8,17 +13,58 @@ const userRouter = require("./natours/routes/userRoutes");
 
 const app = express();
 
-// MIDDELWARES
+// GLOBAL MIDDELWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Dev logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-app.use(express.json());
+
+// Request Limiter
+const limiter = rateLimit({
+  max: 50,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again later.",
+});
+
+app.use("/api", limiter);
+
+// Body parser, reading data from body into req.body
+app.use(
+  express.json({
+    limit: "10kb",
+  })
+);
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter polution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  })
+);
+
+// Serving Static Files
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log(req.headers);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log(req.headers);
+//   next();
+// });
 
 // ROUTES
 app.use("/api/v1/users", userRouter);
--- natours/models/userModel.js
+++ natours/models/userModel.js
@@ -43,6 +43,11 @@ const userSchema = new mongoose.Schema({
   passwordResetToken: String,
   passwordResetExpires: Date,
   passwordConfirm: String,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 });
 
 userSchema.pre("save", async function (next) {
@@ -57,6 +62,11 @@ userSchema.pre("save", function (next) {
   if (!this.isModified("password") || this.isNew) return next();
 
   this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+userSchema.pre(/^find/, function (next) {
+  this.find({ active: { $ne: false } });
   next();
 });
 

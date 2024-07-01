b51fc6d1afd4508bd22496643e09c25e08d53125
Tyler Hargrove <ts.hargrove95@gmail.com>
2024-07-01 14:16:12 -0500

    implemented password changing from user interface as opposed to only via API interactions

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,14 @@
 import "@babel/polyfill";
 import { displayMap } from "./mapbox";
 import { login, logout } from "./login";
-import { updateData } from "./updateSettings";
+import { updateSettings } from "./updateSettings";
 
 // DOM ELEMENTS
 const mapBox = document.getElementById("map");
 const loginForm = document.querySelector(".form--login");
 const logoutButton = document.querySelector(".nav__el--logout");
+const updateDataForm = document.querySelector(".form-user-data");
+const updatePasswordForm = document.querySelector(".form-user-password");
 
 // DELEGATION
 if (mapBox) {
@@ -27,3 +29,29 @@ if (loginForm) {
 }
 
 if (logoutButton) logoutButton.addEventListener("click", logout);
+
+if (updateDataForm)
+  updateDataForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    updateSettings({ name, email }, "data");
+  });
+
+if (updatePasswordForm)
+  updatePasswordForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    document.querySelector("btn--save-password").textContent = "Updating....";
+    const passwordCurrent = document.getElementById("password-current").value;
+    const password = document.getElementById("password").value;
+    const passwordConfirm = document.getElementById("password-confirm").value;
+    await updateSettings(
+      { passwordConfirm, password, passwordConfirm },
+      "password"
+    );
+
+    document.querySelector("btn--save-password").textContent = "Save Password";
+    document.getElementById("password-current").value = "";
+    document.getElementById("password-").value = "";
+    document.getElementById("password-confirm").value = "";
+  });
